ffde691fb8a30aebc5a6aef6c3bbd052940f4295
malfazakki <malfazakki@gmail.com>
2023-05-24 07:59:16 +0700

    update some css and js

--- js/my-project.js
+++ js/my-project.js
@@ -36,12 +36,23 @@ function addProject(event) {
   image = URL.createObjectURL(image[0]);
   const selesai = moment(startDate);
   const mulai = moment(endDate);
-  const durasi = mulai.diff(selesai, "months");
+  const dayDuration = mulai.diff(selesai, "days");
+  const monthDuration = mulai.diff(selesai, "months");
+  const yearsDuration = mulai.diff(selesai, "years");
+  let duration;
+
+  if (monthDuration > 0) {
+    duration = `duration: ${monthDuration} months`;
+  } else if (yearsDuration > 0) {
+    duration = `duration: ${yearsDuration} years`;
+  } else {
+    duration = `duration: $ {dayDuration} days`;
+  }
   console.log(image);
 
   let project = {
     title,
-    durasi,
+    duration,
     description,
     image,
     nodeChecked,
@@ -72,8 +83,8 @@ function renderProject() {
             dataProject[index].title
           }</a>
         </h3>
-        <div class="durasi">
-          <p>Durasi ${dataProject[index].durasi} Bulan</p>
+        <div class="duration">
+          <p>${dataProject[index].duration}</p>
           <p>Posted On:  ${getFullTime(dataProject[index].postAt)}</p>
           <p>by: ${dataProject[index].author}</p>
           <div>
@@ -138,37 +149,3 @@ function getFullTime(time) {
 
   return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
 }
-
-function getDistanceTime(time) {
-  let timeNow = new Date();
-  let timePost = time;
-
-  // waktu sekarang - waktu post
-  let distance = timeNow - timePost; // hasilnya milidetik
-  console.log(distance);
-
-  let milisecond = 1000; // milisecond
-  let secondInHours = 3600; // 1 jam 3600 detik
-  let hoursInDays = 24; // 1 hari 24 jam
-
-  let distanceDay = Math.floor(
-    distance / (milisecond * secondInHours * hoursInDays)
-  ); // 1/86400000
-  let distanceHours = Math.floor(distance / (milisecond * 60 * 60)); // 1/3600000
-  let distanceMinutes = Math.floor(distance / (milisecond * 60)); // 1/60000
-  let distanceSeconds = Math.floor(distance / milisecond); // 1/1000
-
-  if (distanceDay > 0) {
-    return `${distanceDay} Day Ago`;
-  } else if (distanceHours > 0) {
-    return `${distanceHours} Hours Ago`;
-  } else if (distanceMinutes > 0) {
-    return `${distanceMinutes} Minutes Ago`;
-  } else {
-    return `${distanceSeconds} Seconds Ago`;
-  }
-}
-
-// setInterval(function () {
-//   renderProject();
-// }, 10000);
